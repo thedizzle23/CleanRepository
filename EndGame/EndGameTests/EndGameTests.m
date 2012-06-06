bc7e46cf8ab5ef12e6b63aefe3b5ff4de0b99ff4
Robert DeNicola <thedizzle23@gmail.com>
2012-06-06 13:45:17 -0400

    then is good

---
+++ EndGame/EndGameTests/EndGameTests.m
@@ -0,0 +1,32 @@
+//
+//  EndGameTests.m
+//  EndGameTests
+//
+//  Created by Robert DeNicola on 6/6/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "EndGameTests.h"
+
+@implementation EndGameTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in EndGameTests");
+}
+
+@end
